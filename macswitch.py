3db30258daa1fa5f3f7e4a52c7dbbc8ae5e9d589
Zakarye Ridoine <168565323+secusavvy@users.noreply.github.com>
2024-09-05 20:32:56 +0100

    Update macswitch.py

--- macswitch.py
+++ macswitch.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 
 import subprocess
-import optparse
+import argparse
 
-parser = optparse.OptionParser()
+def change_mac(interface, new_mac):
+    try:
+        print(f"[+] Changing MAC Address for {interface} to {new_mac}")
+        subprocess.check_call(["ifconfig", interface, "down"])
+        subprocess.check_call(["ifconfig", interface, "hw", "ether", new_mac])
+        subprocess.check_call(["ifconfig", interface, "up"])
+        print(f"[+] MAC Address changed successfully to {new_mac}")
 
-parser.add_option("-i", "--interface" , dest= "interface", help="Interface to change MAC Address")
-parser.add_option("-m", "--mac" , dest= "new_mac", help="New MAC Address")
+    except subprocess.CalledProcessError:
+        print(f"[-] Error occurred while changing MAC address for {interface}.")
+        return
 
-(options, arguments) = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description="Change the MAC address of a network interface.")
+    parser.add_argument("-i", "--interface", dest="interface", required=True, help="Interface to change MAC Address")
+    parser.add_argument("-m", "--mac", dest="new_mac", required=True, help="New MAC Address")
+    options = parser.parse_args()
+    change_mac(options.interface, options.new_mac)
 
-interface = options.interface
-new_mac = options.new_mac
-
-print("[+] Changing MAC Address for " + interface + " to " + new_mac)
-
-subprocess.call(["ifconfig", interface , "down"])
-subprocess.call(["ifconfig", interface , "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface , "up"])
+if __name__ == "__main__":
+    main()
